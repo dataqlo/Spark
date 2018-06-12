fbc8e11821dd0eecf3c7d7198c5bb91e277e9b9e
Abhishek Solanki <abhisolanki94@gmail.com>
2018-06-12 23:22:00 +0530

    day9

--- spark/training/day9.scala
+++ spark/training/day9.scala
@@ -2,3 +2,5 @@
 // scala.collection.immutable -> never changes after it is created
 // scala.collection.mutable -> contains some operations that changes collection in place.
 
+// Traversable  has one abstract method foreach 
+// Iterable has an abstract method Iterator, next method can be called 
