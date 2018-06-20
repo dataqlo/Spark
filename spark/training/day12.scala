c307489ab385e640771a6e9787d43a93759300e0
Abhishek Solanki <abhisolanki94@gmail.com>
2018-06-20 07:36:34 +0530

    day12
    Avro

--- spark/training/day12.scala
+++ spark/training/day12.scala
@@ -1,4 +1,5 @@
 // File Formats
+// AVRO line (4-80)
 
 // 1. AVRO
 // The file is defined by avro schema; while searializing / deserializing data, schema is used. Schema is sent with data or is stored 
@@ -15,10 +16,11 @@
 //avro-tools fromjson --schema-file member.avsc member.json > member.avro
 // hadoop fs -put member.avro dataset/avro/
 
+// the databricks library can be specified externally by using --package option in spark-shell or spark-submit
+// bin/spark-shell --packages com.databricks:spark-avro_2.11:4.0.0
+
 
 val sqlContext = new org.apache.spark.sql.SQLContext(sc)
-// import needed for the .avro method to be added
-import com.databricks.spark.avro._
 
 val df = sqlContext.read.
  format("com.databricks.spark.avro").
@@ -26,6 +28,8 @@ val df = sqlContext.read.
 
 //df: org.apache.spark.sql.DataFrame = [id: string, name: string, timestamp: bigint]
 
+// import needed for the .avro method to be added
+import com.databricks.spark.avro._
 val df1 = sqlContext.read.avro("dataset/avro/member.avro")
 //df1: org.apache.spark.sql.DataFrame = [id: string, name: string, timestamp: bigint]
 
@@ -37,3 +41,40 @@ val df1 = sqlContext.read.avro("dataset/avro/member.avro")
 //|1002|    Jane Doe|1366489544|
 //|1002|Jason Bourne|1366489549|
 //+----+------------+----------+
+
+// Specifying avero schema
+import org.apache.avro.Schema
+import java.io._
+val schema = new Schema.Parser().parse(new File("/home/cloudera/Desktop/dataset/avro/member.avsc"))
+val df = sqlContext.read.
+ format("com.databricks.spark.avro").
+ option("avroSchema",schema.toString).
+ load("hdfs://localhost:8020/user/cloudera/dataset/avro/member.avro")
+df.show
+
+// Compression Options
+// 3 types of compression : uncompressed, snappy and deflate ( in deflate you can specify the level)
+import com.databricks.spark.avro._
+
+sqlContext.setConf("spark.sql.avro.compression.codec","deflate") // Program: sqlContext.conf.set("spark.sql.avro.compression.codec","deflate")
+sqlContext.setConf("spark.sql.avro.deflate.level","5") // Program: sqlContext.conf.set("spark.sql.avro.deflate.level","5")
+val df = sqlContext.read.avro("dataset/avro/member.avro")
+df.write.avro("dataset/avro/member_compressed_deflate.avro")
+//rw-r--r--   1 cloudera cloudera          0 2018-06-19 18:45 hdfs://localhost:8020/user/cloudera/dataset/avro/member_compressed_deflate.avro/_SUCCESS
+//-rw-r--r--   1 cloudera cloudera        307 2018-06-19 18:45 hdfs://localhost:8020/user/cloudera/dataset/avro/member_compressed_deflate.avro/part-r-00000-8be18a9b-17df-48c5-9ae9
+
+// Avro write partition 
+df.write.partitionBy("id").avro("dataset/avro/member_partition/")
+
+//-rw-r--r--   1 cloudera cloudera          0 2018-06-19 18:51 hdfs://localhost:8020/user/cloudera/dataset/avro/member_partition/_SUCCESS
+//drwxr-xr-x   - cloudera cloudera          0 2018-06-19 18:51 hdfs://localhost:8020/user/cloudera/dataset/avro/member_partition/id=1001
+//drwxr-xr-x   - cloudera cloudera          0 2018-06-19 18:51 hdfs://localhost:8020/user/cloudera/dataset/avro/member_partition/id=1002
+
+
+// Specifying avro name and namespace via parameter
+val name = "AvroParameterTest"
+val namespace = "com.dataqlo.spark.avro"
+val parameters = Map("recordName" -> name, "recordNamespace" -> namespace)
+
+df.write.options(parameters).avro("dataset/avro/member_param_test")
+// the output file will now have new name and namespace values
