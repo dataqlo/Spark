06c9952254ba209c27de5b4da71229c8fbca325b
Abhishek Solanki <abhisolanki94@gmail.com>
2018-06-25 08:46:45 +0530

    day13
    DStream

--- spark/training/day13.scala
+++ spark/training/day13.scala
@@ -30,4 +30,21 @@ wordCounts.print()
 ssc.start()             // Start the computation
 ssc.awaitTermination()  // Wait for the computation to terminate
 
+// points to remember
+// 1. Once the streaming context has started, no new streaming computation can be added or removed
+// 2. Once the context has been stoped it can not be restared
+// 3. Only one Streaming context can be active in JVM
+// 4. stop() on Streaming Context also stops SparkContext. This is handled by adding optional parameter in stop()
+// stopSparkContext to false
+// 5. A SparkContext can be re-used to create multiple StreamingContexts, as long as the previous StreamingContext is stopped 
+// (without stopping the SparkContext) before the next StreamingContext is created.
+// 5. While running streaming locally do not use local, local[1] as master URL, this means only one theread will receives data and
+// no thread for processing the received data. Set min theread to 2
+// 6. The no. of core for spark streaming should be more than no. of receivers, else the system will receive data but not be able to process
 
+
+// DStream
+// DStream contains data from a certain interval
+// Any operation on DStream translates to the underlying RDDs
+// Input DStream: stream of data received from streaming source except file stream is associated with a "Receiver" Object
+// 2 Category of built in source - BASIC: File System, Socket Connection ADVANCE: Kafka, Flume, Twitter
