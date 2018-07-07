178d74ce35115f6caaaed04ebbdb363e31918d73
Abhishek Solanki <abhisolanki94@gmail.com>
2018-07-08 01:43:35 +0530

    day14
    kafka theory

---
+++ spark/training/day14.scala
@@ -0,0 +1,10 @@
+// Kafka
+
+// Kafka runs on multiple cluster and can span across multiple datacenteres 
+// Topics: stream of records stored in category on kafka cluster
+// Eack record is stored in KV pair and a timestamp
+// Producers: allows application to publish stream of records to topics
+// Consumer: allows application to subscribe to one or more topics 
+// Stream: allows application to act as producer and Consumer, effectively transformation input stream to output stream
+// Connector: eg. relationl db Connector, captures changes to table and publish to a topics
+
