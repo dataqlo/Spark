637c3adfce31f10d276e91c6830d5d9b4476c77e
Abhishek Solanki <abhisolanki94@gmail.com>
2018-07-08 22:37:50 +0530

    day14
    kafka theory

--- spark/training/day14.scala
+++ spark/training/day14.scala
@@ -8,3 +8,29 @@
 // Stream: allows application to act as producer and Consumer, effectively transformation input stream to output stream
 // Connector: eg. relationl db Connector, captures changes to table and publish to a topics
 
+
+// Topics 
+// cataegory of feed name to which records will be published.
+// topics are multi-subscribers
+// there is a partition log for each topic on cluster
+// each record is assigned a sequence id called offset, which uniquely identifies record in partition
+// retention period can be configured, for data to be available in topic
+// the offset is controlled by consumer
+
+
+// start the server 
+// sh /usr/lib/kafka/bin/kafka-server-start.sh /usr/lib/kafka/config/server.properties
+//  --override delete.topic.enable=true \
+//  --override broker.id=100 \
+
+// create topic 
+// kafka-topics --create --zookeeper localhost:2181 --replication-factor 3 --partitions 1 --topic topic_name_1
+
+// list available topics
+// kafka-topics --zookeeper localhost:2181 --list
+
+// describing topic
+// kafka-topics --zookeeper localhost:2181 --describe --topic topic_name_1
+
+// delete topic (  --override delete.topic.enable=true - should be used while starting the kafka server to delete topic )
+// kafka-topics --zookeeper localhost:2181 --delete --topic topic_name_1
