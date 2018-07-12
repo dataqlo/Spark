3786ba26a281f17fc0a8cdc14a7f415cd6a8c6c3
Abhishek Solanki <abhisolanki94@gmail.com>
2018-07-12 08:29:42 +0530

    project: BankOfData update
    data pushed to respective kafka topics.

---
+++ project/BankOfData/fetch_data.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# Author: github.com/AbhishekSolanki
+
+receiveData=true
+kill -9 `cat logs/kafka_server.pid`
+echo "`date` INFO: Starting kafka server"
+nohup sh /usr/lib/kafka/bin/kafka-server-start.sh /usr/lib/kafka/config/server.properties > logs/kafka_server.out 2>&1 &
+echo $! > logs/kafka_server.pid
+echo "`date` INFO: Kafka server started !"
+
+echo "`date` INFO: Creating Kafka topics"
+kafka-topics --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic bod-q-mastercard
+kafka-topics --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic bod-q-visa
+kafka-topics --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic bod-q-amex
+echo "`date` INFO: Kafka Topics Created !"
+
+while $receiveData
+do
+ payload=$(curl -s -get http://localhost:8080/BOD/rest/Transaction/data/)
+ # exit condition when all the data is read
+ echo $payload | grep -q "HTTP Status 500"
+ if [ $? -eq 0 ]; then echo "end"; receiveData=false; break; fi
+ cardType="${payload:0:1}"
+ if [ $cardType == "<" ];then
+ 	#echo "kafka_mastercard_q: "$payload
+        echo $payload | kafka-console-producer --broker-list localhost:9092 --topic bod-q-mastercard
+ elif [ $cardType == "[" ]; then
+ 	#echo "kafka_visa_q: "$payload
+        echo $payload | kafka-console-producer --broker-list localhost:9092 --topic bod-q-visa
+ else
+ 	#echo "kafka_amex_q: "$payload
+        echo $payload | kafka-console-producer --broker-list localhost:9092 --topic bod-q-amex
+ fi
+echo "`date` INFO: payload push to kafka topic"
+sleep 0.25
+done
