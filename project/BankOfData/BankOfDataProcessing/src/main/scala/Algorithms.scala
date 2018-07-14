87b95ce33e8bf83728063f8a0e60dffaea63c956
Abhishek Solanki <abhisolanki94@gmail.com>
2018-07-14 21:04:32 +0530

    project: BOD added comments

--- project/BankOfData/BankOfDataProcessing/src/main/scala/Algorithms.scala
+++ project/BankOfData/BankOfDataProcessing/src/main/scala/Algorithms.scala
@@ -1,49 +1,62 @@
+/*
+ * Author: https://github.com/AbhishekSolanki
+ * Date: 14th July 2018
+ * Description: contains luhn algorithm for card number validation and data processing method based on record type
+*/
 import scala.collection.immutable.List
 import net.liftweb.json._
 import net.liftweb.json.JsonDSL._
 
-
+// Serializable is required for class otherwise "Task not Serializable: java.io.NotSerializableException" will occur
 @SerialVersionUID(123L)
 class Algorithms  extends Serializable{
 
-	def luhmAlgoCardValidation(cardNumber:String) : String = {
-		val originalCardNumber = cardNumber.reverse.toList
-				var sumOfCardNumber = 0
-				originalCardNumber.zipWithIndex.foreach{
-				case (elem,index) => {
-					if(index==0) sumOfCardNumber+=elem.toString.toInt
-							else
-								if(index%2!=0){
-									sumOfCardNumber += (elem.asDigit*2).toString.toList.map(_.toString.toInt).sum
-								} else {
-									sumOfCardNumber += elem.toString.toInt
-								}
-				}}
-		if ( sumOfCardNumber % 10 == 0 ) "valid" else "invalid"
-	}
+  // validates the card number based on luhn algorithm and returns the string output "valid" / "invalid"
+	def luhnAlgoCardValidation(cardNumber:String) : String = {
+	  val originalCardNumber = cardNumber.reverse.toList
+		var sumOfCardNumber = 0
+		// zipWithIndex method returns (element,index) for list
+		originalCardNumber.zipWithIndex.foreach{
+	    case (elem,index) => {
+	      if(index==0) sumOfCardNumber+=elem.toString.toInt
+	      else
+	        if(index%2!=0){
+	          sumOfCardNumber += (elem.asDigit*2).toString.toList.map(_.toString.toInt).sum
+	          }
+	        else {
+	          sumOfCardNumber += elem.toString.toInt
+					}
+				}
+	    }
+	  if ( sumOfCardNumber % 10 == 0 ) "valid" else "invalid"
+	 }
 	
+	// process the data received from kafka queue, based of format it returns List(IIN:String, cardNumber:String, valid:String)
 	def dataProcess(dataPayLoad:String): List[String] = {
 	  if ( dataPayLoad.charAt(0) == '<' ) {
-						//	XML data
-						val xmlDataPayLoad = xml.XML.loadString(dataPayLoad)
-								val IIN = (xmlDataPayLoad \\"IssuingNetwork").text
-								val cardNumber = (xmlDataPayLoad \\"CardNumber").text
-								val valid = this.luhmAlgoCardValidation(cardNumber)
-								List(IIN,cardNumber,valid)
-					} else if ( dataPayLoad.charAt(0) == '[') {
-						// JSON data
-						val json = parse(dataPayLoad)
-								val IIN = compactRender(json\"CreditCard"\"IssuingNetwork").replace("\"", "")
-								val cardNumber = compactRender(json\"CreditCard"\"CardNumber")
-								val valid = this.luhmAlgoCardValidation(cardNumber)
-								List(IIN,cardNumber,valid)
-					} else {
-						// CSV data
-						val csvDataPayLoad = dataPayLoad.split(",")
-								val IIN = csvDataPayLoad(0)
-								val cardNumber = csvDataPayLoad(1)
-								val valid = this.luhmAlgoCardValidation(cardNumber)
-								List(IIN,cardNumber,valid)
-					}
+	    //	XML data
+			val xmlDataPayLoad = xml.XML.loadString(dataPayLoad)
+			val IIN = (xmlDataPayLoad \\"IssuingNetwork").text
+			val cardNumber = (xmlDataPayLoad \\"CardNumber").text
+			val valid = this.luhnAlgoCardValidation(cardNumber)
+			List(IIN,cardNumber,valid)
+			}
+	  else if ( dataPayLoad.charAt(0) == '[') {
+	    // JSON data
+	    // for parsing json external Lift-Json library is used with XPATH option
+			val json = parse(dataPayLoad)
+			val IIN = compactRender(json\"CreditCard"\"IssuingNetwork").replace("\"", "")
+			val cardNumber = compactRender(json\"CreditCard"\"CardNumber")
+			val valid = this.luhnAlgoCardValidation(cardNumber)
+			List(IIN,cardNumber,valid)
+			} 
+	  else {
+			// CSV data
+			val csvDataPayLoad = dataPayLoad.split(",")
+			val IIN = csvDataPayLoad(0)
+			val cardNumber = csvDataPayLoad(1)
+			val valid = this.luhnAlgoCardValidation(cardNumber)
+			List(IIN,cardNumber,valid)
+		}
 	}
 }
--- project/BankOfData/BankOfDataProcessing/src/main/scala/App.scala
+++ project/BankOfData/BankOfDataProcessing/src/main/scala/App.scala
@@ -1,3 +1,8 @@
+/*
+ * Author: https://github.com/AbhishekSolanki
+ * Date: 14th July 2018
+ * Description: Main file for the application which connects to kafka topics and receives data
+*/
 import org.apache.spark._
 import org.apache.spark.streaming._
 import org.apache.spark.streaming.StreamingContext._
@@ -9,25 +14,31 @@ import _root_.kafka.serializer.StringDecoder
 
 object App {
 	def main(args: Array[String]): Unit = {
-			val conf = new SparkConf().setMaster("local[2]").setAppName("BOD_kafka_process")
-					val ssc = new StreamingContext(conf, Seconds(10))
+	  // create spark configuration with 2 threads and app name to be visible in yarn
+	  val conf = new SparkConf().setMaster("local[2]").setAppName("BOD_kafka_process")
+	  // create spark streaming context with batch interval of 10 seconds
+		val ssc = new StreamingContext(conf, Seconds(10))
 
-					val topicSet = Set("bod-q-amex","bod-q-mastercard","bod-q-visa")
-					val kafkaParams = Map[String, String](
-							ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG -> "localhost:9092",
-							ConsumerConfig.GROUP_ID_CONFIG -> "100"
-							)
+	  // set of topics to which consumer will be subscribed to 
+		val topicSet = Set("bod-q-amex","bod-q-mastercard","bod-q-visa")
+		// default kafka parameters which specifies server details
+		val kafkaParams = Map[String, String](
+		    ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG -> "localhost:9092",
+				ConsumerConfig.GROUP_ID_CONFIG -> "100"
+		    )
 
-					val payLoad= KafkaUtils.createDirectStream[String, String,StringDecoder,StringDecoder](
-							ssc, kafkaParams, topicSet)
-
-					val algoObj = new Algorithms()
-
-					payLoad.print()
-					payLoad.map(dataPayLoad=>algoObj.dataProcess(dataPayLoad._2)).print
-
-					ssc.start()
-					ssc.awaitTermination()
-
+		// data received from all the topics in form of (null,data)
+		val payLoad= KafkaUtils.createDirectStream[String, String,StringDecoder,StringDecoder](
+		    ssc, kafkaParams, topicSet)
+		// creating object of Algorithm class which contains logic, class should be serializables
+		val algoObj = new Algorithms()
+	  // prints the first 10 records, just for dev testing purpose
+	  payLoad.print()
+	  // for each record in topic we apply map function to parse the receive data using dataProcess method
+		payLoad.map(dataPayLoad=>algoObj.dataProcess(dataPayLoad._2)).print
+		// start the computation
+		ssc.start()
+		// waiting for computation to terminate
+		ssc.awaitTermination()
 	}
 }
